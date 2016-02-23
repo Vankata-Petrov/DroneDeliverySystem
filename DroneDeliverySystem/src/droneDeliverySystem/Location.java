98e07395da2bc83f7f9001e31ed8e610c4d72c42
Georgi Dankov <gddankov@gmail.com>
2016-02-23 14:40:17 +0200

    add basic classes

---
+++ DroneDeliverySystem/src/droneDeliverySystem/Location.java
@@ -0,0 +1,20 @@
+package droneDeliverySystem;
+
+public class Location {
+	
+	private int xCoordinate;
+	private int yCoordinate;
+	
+	public Location(int xCoordinate, int yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
+
+	public int getXCoordinate() {
+		return xCoordinate;
+	}
+
+	public int getYCoordinate() {
+		return yCoordinate;
+	}
+}
